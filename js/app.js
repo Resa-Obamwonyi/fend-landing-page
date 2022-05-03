7798f38d3e1993a3a234a588f42d96c0fd16afb3
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-05-03 15:38:49 +0100

    feat-dynamically added nav bar

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
-*/
 
 /**
  * Comments should be present at the beginning of each procedure and class.
@@ -23,10 +9,13 @@
  *
 */
 let navContainer = document.getElementById("navbar__list");
-let sectionOne = document.getElementById("section1");
-let sectionTwo = document.getElementById("section2");
-let sectionThree = document.getElementById("section3");
-let sectionFour = document.getElementById("section4");
+let sectionGroup = document.getElementsByTagName("section");
+
+console.log(sectionGroup);
+// let sectionOne = document.getElementById("section1");
+// let sectionTwo = document.getElementById("section2");
+// let sectionThree = document.getElementById("section3");
+// let sectionFour = document.getElementById("section4");
 
 /**
  * End Global Variables
@@ -43,9 +32,28 @@ let sectionFour = document.getElementById("section4");
 */
 
 // build the nav
+function navBuilder() {
+    // for each section in the section group
+    for (let i = 0; i < sectionGroup.length; i++) {
+        // Create a list element
+        let listItem = document.createElement("li");
+        // add the menu__link class
+        listItem.classList.add("menu__link");
+        // Append text specific to each section to the list element
+        listItem.innerHTML = sectionGroup[i].dataset.nav;
+        // Append list element to ul 
+        navContainer.appendChild(listItem);
+    }
+}
 
 
 // Add class 'active' to section when near top of viewport
+function addClassActive(){
+    // for each section in the section group
+    for(let i=0; i<sectionGroup.length; i++){
+        
+    }
+}
 
 
 // Scroll to anchor ID using scrollTO event
@@ -57,7 +65,8 @@ let sectionFour = document.getElementById("section4");
  *
 */
 
-// Build menu 
+// Build menu
+navBuilder(); 
 
 // Scroll to section on link click
 
